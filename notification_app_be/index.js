5ff2560ebd48d1da8ea84253071e631d8e06414f
Prashant Kumar Singh <ps7027804@gmail.com>
2026-06-11 13:44:57 +0530

    final - tokens updated and tested

--- notification_app_be/index.js
+++ notification_app_be/index.js
@@ -3,7 +3,7 @@ const { Log } = require('../logging_middleware/index');
 const app = express();
 app.use(express.json());
 
-const TOKEN = "APNA_BEARER_TOKEN_YAHAN_DAALO";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJwcmFzaGFudEBtcGdpLmVkdS5pbiIsImV4cCI6MTc4MTE2NjQzNSwiaWF0IjoxNzgxMTY1NTM1LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiMmIzMTYzN2MtOWRhNi00ZjdhLTgzNGEtNTVhMjdlNTdlMTllIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoicHJhc2hhbnQga3VtYXIgc2luZ2giLCJzdWIiOiI4ZmQ0Y2MzNC1jYWJjLTQ2ZmItOWI1Mi02MGYwMjBjMjRlYWMifSwiZW1haWwiOiJwcmFzaGFudEBtcGdpLmVkdS5pbiIsIm5hbWUiOiJwcmFzaGFudCBrdW1hciBzaW5naCIsInJvbGxObyI6IjIzMDM0OTE1MzAwNzUiLCJhY2Nlc3NDb2RlIjoiQkFWRFNoIiwiY2xpZW50SUQiOiI4ZmQ0Y2MzNC1jYWJjLTQ2ZmItOWI1Mi02MGYwMjBjMjRlYWMiLCJjbGllbnRTZWNyZXQiOiJaanpUQVl6cEtHWmhIWUJDIn0.NW14-yRqE1pPEpKhP9X7t4j5W91lrN5hfxSuNZZ5aZU";
 
 // Fetch notifications from Affordmed API
 async function getNotifications() {
@@ -13,7 +13,7 @@ async function getNotifications() {
     headers: { "Authorization": `Bearer ${TOKEN}` }
   });
   const data = await response.json();
-  return data.notifications;
+  return data.notifications || [];
 }
 
 // Priority scoring function
